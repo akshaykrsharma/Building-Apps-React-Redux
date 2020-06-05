855399457c2008f968f55c7cc5bb7ebbf03d3f64
Akshay Sharma <akshay@punchh.com>
2020-06-05 09:49:40 +0530

    Different Types of Component Example
    1. Functional Component
    2. Class based Component
    3. Arrow Function 
    4. Hooks

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { render } from 'react-dom';
-
-function FirstPage() {
-	return <h1>Hello World</h1>;
+function FunctionalComponent() {
+	return <h1>Functional Component</h1>;
 }
 
-render(<FirstPage />, document.getElementById('app'));
+const ArrowFunction = (props) => <h3>Arrow Function</h3>;
+
+const HooksFunction = function () {
+	const [count, setCount] = useState(0);
+	return (
+		<div>
+			<p>Hooks: Your count {count}</p>
+			<button
+				style={{ width: 100, height: 44 }}
+				onClick={() => {
+					setCount(count + 1);
+				}}
+			>
+				Click Me
+			</button>
+		</div>
+	);
+};
+
+class ClassComponent extends Component {
+	render() {
+		return <h2>Class Component</h2>;
+	}
+}
+
+render(
+	<div>
+		<FunctionalComponent />
+		<ClassComponent />
+		<ArrowFunction />
+		<HooksFunction />
+	</div>,
+	document.getElementById('app')
+);
