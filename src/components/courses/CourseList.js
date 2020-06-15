efbe6998dd4bc6f3083c9b007228d0d0aab9deae
Akshay Sharma <sharma.akshay5@gmail.com>
2020-06-15 13:39:51 +0530

    Form Creator Add Data in DB Update Data of DB

--- src/components/courses/CourseList.js
+++ src/components/courses/CourseList.js
@@ -1,19 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 const CourseList = ({ courses }) => (
 	<table className="table">
 		<tr>
+			<th />
 			<th>Title</th>
 			<th>Author</th>
 			<th>Category</th>
 		</tr>
 		{courses.map((item) => (
 			<tr key={item.title}>
-				<td>{item.title}</td>
+				<td>
+					<a href={'http://pluralsight.com/courses/' + item.slug}>Watch</a>
+				</td>
+				<td>
+					<Link to={'course/' + item.slug}>{item.title}</Link>
+				</td>
 				<td>{item.authorName}</td>
 				<td>{item.category}</td>
 			</tr>
 		))}
 	</table>
 );
+
+CourseList.propTypes = {
+	courses: PropTypes.array.isRequired,
+};
 
 export default CourseList;
--- src/redux/actions/courseAction.js
+++ src/redux/actions/courseAction.js
@@ -1,10 +1,17 @@
-import { CREATE_COURSE, LOAD_COURSE_SUCCESS } from './actionTypes';
+import { CREATE_COURSE, LOAD_COURSE_SUCCESS, CREATE_COURSE_SUCCESS, UPDATE_COURSE_SUCCESS } from './actionTypes';
 import * as courseApi from '../../api/courseApi';
 export function createCourse(course) {
 	return { type: CREATE_COURSE, payload: course };
 }
 export function loadCourseSuccess(course) {
 	return { type: LOAD_COURSE_SUCCESS, payload: course };
+}
+
+export function createCourseSuccess(course) {
+	return { type: CREATE_COURSE_SUCCESS, payload: course };
+}
+export function updateCourseSuccess(course) {
+	return { type: UPDATE_COURSE_SUCCESS, payload: course };
 }
 
 // Thunk a function which returns another function, It's the work flow of thunk
@@ -14,6 +21,19 @@ export function loadCourse() {
 			.getCourses()
 			.then((courses) => {
 				dispatch(loadCourseSuccess(courses));
+			})
+			.catch((exp) => {
+				throw exp;
+			});
+	};
+}
+
+export function saveCourse(course) {
+	return function (dispatch) {
+		return courseApi
+			.saveCourse(course)
+			.then((saveCourse) => {
+				course.id ? dispatch(updateCourseSuccess(saveCourse)) : dispatch(createCourseSuccess(saveCourse));
 			})
 			.catch((exp) => {
 				throw exp;
